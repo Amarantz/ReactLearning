53772776a06e9e0e1bfc6c6f27163ae76e7a4445
Blaine Callahan <bcallahan@contractor.youniqueproducts.com>
2018-07-23 11:07:30 -0600

    Learning Shit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import ReactDom from 'react-dom';
 
-import App from './components/app';
-import reducers from './reducers';
+const API_KEY = 'AIzaSyBv3UuHlTlS_N7ffibVhP33lnMWRiYfMpM';
+// create a new component. this component should produce some html
+const App = () => {
+    return <div>Hi!</div>;
+}
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// Take this component's generated HTML and put in on the page (in the DOM)
+ReactDom.render(<App />, document.querySelector('.container'));
