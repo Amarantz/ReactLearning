2621a0e3f9a7505d1c23b51b2cf61cff84c6888a
Blaine Callahan <bcallahan@contractor.youniqueproducts.com>
2018-07-24 18:17:05 -0600

    Lawl

--- src/index.js
+++ src/index.js
@@ -1,10 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDom from 'react-dom';
+import YTSearch from'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyBv3UuHlTlS_N7ffibVhP33lnMWRiYfMpM';
+
 // create a new component. this component should produce some html
-const App = () => {
-    return <div>Hi!</div>;
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { 
+            videos: [],
+            selectedVideo: null
+        };
+
+        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+            this.setState({ 
+                videos: videos,
+                selectedVideo: videos[0] 
+            });
+        });
+    }
+
+    render () {
+        return(
+            <div>
+                <SearchBar />
+                <VideoDetail video={this.state.selectedVideo} />
+                <VideoList 
+                    onVideoSelect={selectedVideo => this.setState({selectedVideo})} 
+                    videos={this.state.videos} />
+            </div>
+        );
+    }
 }
 
 // Take this component's generated HTML and put in on the page (in the DOM)
